111fbd99e5443eb1724f85ca2252007905d4f7e3
Parfaite <parfaitehounkpatchi@gmail.com>
2025-01-14 08:00:17 -0500

    commit4

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -7,14 +7,6 @@ function ProductList() {
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
 
-    const handleAddToCart = (product) => {
-        dispatch(addItem(product));
-        setAddedToCart((prevState) => ({
-           ...prevState,
-           [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
-         }));
-    };
-
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -256,6 +248,9 @@ function ProductList() {
         e.preventDefault();
         setShowCart(false);
     };
+
+    
+
     return (
         <div>
             <div className="navbar" style={styleObj}>
